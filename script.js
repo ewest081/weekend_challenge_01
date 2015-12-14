107611bc8c409cba8e62940cbe994fa3d8a7c2ec
Liz Westberg <ewest081@gmail.com>
2015-12-14 10:00:55 -0600

    Fix several issues, clean up code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 var employeeArray = [];
-var salarySum = 0;
 
 $(document).ready(function(){
 	$("#employeedata").on('submit',function(event){
@@ -16,35 +15,9 @@ $(document).ready(function(){
 		console.log(values);
 		appendDom(values);
 
-		salarySum += parseInt(values.employeesalary);
-
-		// employeeArray.push(values);
-
-		// function payroll(array){
-		// totalPayroll += parseInt(array[2]/12);
-		// };
-		
-		// console.log(employeeArray);
-		// console.log(payroll(employeeArray));
-		// console.log(totalPayroll);
-		// var payroll = function(array){
-		// 	var totalPayroll = 0;
-		// 	console.log(typeof totalPayroll);
-		// 	for (i = 0; i < employeeArray.length; i++) {
-		// 		totalPayroll += (parseInt(employeeArray[i].emoloyeesalary)/12);
-		// 	console.log(typeof totalPayroll);
-
-		// 	}
-		// 	console.log(typeof totalPayroll);
-
-		// 	return totalPayroll;
-		// };
-//I've moved this damn function around and changed it so many times... I simply cannot get it to work properly. So I'm going to cry myself to sleep and hopefully get some rest and look at it again in the morning. Transcendence through suffering!
 		employeeArray.push(values);
+
 		console.log(employeeArray);
-		// console.log(payroll(employeeArray));
-		console.log(salarySum);
-		return salarySum;
 	})
 });
 
@@ -56,34 +29,22 @@ function appendDom(object){
 	$el.append("<p>" + object.employeenumber + "</p>");
 	$el.append("<p>" + object.employeetitle + "</p>");
 	$el.append("<p>" + object.employeesalary + "</p>");
-	// $el.append("<p>" + salarySum + "</p>");
 
-	// function payroll(object){
-	// 	totalPayroll += parseInt(object.employeesalary);
-	// };
-
-	// $el.apend("<p>" + totalPayroll + "</p>");
 };
 
+function totalSalary(){
+	var salarySum = 0;
 
-// function payroll(array){
-// 	totalPayroll += parseInt(employeeArray[2]/12);
+	for(var i = 0; i < employeeArray.length; i++){
+		var object = employeeArray[i];
+		salarySum += parseInt(object.employeesalary);
+	}
 
-// 	// $("total").append("<div></div>");
-// 	// 	var $el = $("#total").children().last();
+	salarySum /= 12;
 
-// 	// $el.append("<p>" + totalPayroll + "</p>");
+	$("#totalcontainer").text("Total Monthly Salary: " + salarySum);
 
-// 	console.log(totalPayroll);
-// };
-
-//why won't it woooooorrrrrk!!!
-
-function totalSalary(input){
-	$("#container").append("<div></div>");
-	var $la = $("#container").children().last();
-
-	$la.append("<p> Total monthly payroll is " + (salarySum/12) + "</p>");
+	console.log(salarySum);
 };
 
 
